4644a327572221afbe2bed0ca65ebba7691ec630
Luna Sofie <nore0404@gmail.com>
2024-01-23 15:22:48 +0100

    In Progress

---
+++ Weektasks/Oppgave 5.cpp
@@ -0,0 +1,43 @@
+
+/**     Project name
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oppgave 5.cpp
+ *      @date 23-Jan-24
+ *
+ *      @author Luna S.
+ *      @alias LunaMellow
+ */
+
+// Includes
+#include <iostream>
+
+// Namespace
+using namespace std;
+
+// Const Declarations
+const int STRLEN = 40;               ///< Max.tekstlengde.
+const int MAXFORELESNINGER = 10;     ///< Max.antall forelesninger.
+
+// Struct Declarations
+struct Forelesning {
+    char emne[STRLEN/2],
+            foreleser[STRLEN],
+            sted[STRLEN/2];
+    int timeStart, minuttStart,
+            timeSlutt, minuttSlutt;
+};
+
+/**
+ *      Main Program starts here
+ */
+int main() {
+
+    // Code comes here soon
+
+    return 0;
+}
