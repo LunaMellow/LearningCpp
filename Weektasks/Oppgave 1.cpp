f59f121202b50b6aea3563549fb163eec4b3bda5
Luna Sofie <nore0404@gmail.com>
2024-01-23 14:04:37 +0100

    Initial commit

---
+++ Weektasks/Oppgave 1.cpp
@@ -0,0 +1,16 @@
+
+/**     Project name
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oppgave 1.cpp
+ *      @date 23-Jan-24
+ *
+ *      @author Luna S.
+ *      @alias LunaMellow
+ */
+
+// Includes
